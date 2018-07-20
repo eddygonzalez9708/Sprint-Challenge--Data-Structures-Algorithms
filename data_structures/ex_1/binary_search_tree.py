394e1441a78150947e0cecb55486aefd3d4badad
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-07-20 15:39:23 -0400

    Implemented iterative approach for depth first traversal

--- data_structures/ex_1/binary_search_tree.py
+++ data_structures/ex_1/binary_search_tree.py
@@ -5,13 +5,33 @@ class BinarySearchTree:
     self.right = None
 
   def depth_first_for_each(self, cb):
-    # Recursive 
-    # call the cb on the current BST mode
-    cb(self.value)
-    if self.left: 
-      self.left.depth_first_for_each(cb)
-    if self.right: 
-      self.right.depth_first_for_each(cb)
+    
+    #Recursive 
+    #call the cb on the current BST mode
+    
+    # cb(self.value)
+    # if self.left: 
+    #   self.left.depth_first_for_each(cb)
+    # if self.right: 
+    #   self.right.depth_first_for_each(cb)
+
+    # Iterative 
+    
+    # stack = []
+    # # append the root node of our BST
+    # stack.append(self)
+    # # iterate through the elements in the stack 
+    # while len(stack):
+    #   # pop off the top-most stack element 
+    #   current_node = stack.pop()
+    #   # check to see if this node has a right child
+    #   if current_node.right: 
+    #     stack.append(current_node.right)
+    #   # check to see if this node has a left child 
+    #   if current_node.left: 
+    #     stack.append(current_node.left)
+    #   # don't forget to call the callback
+    #   cb(current_node.value)
 
   def breadth_first_for_each(self, cb):
     pass
